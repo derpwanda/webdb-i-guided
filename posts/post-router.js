6a16a8bed319ced475be2832d4b701ee5d72aaab
derpWanda <derpWanda@gmail.com>
2019-12-17 20:58:07 -0600

    complete postrouter, need add validateId midware

--- posts/post-router.js
+++ posts/post-router.js
@@ -3,24 +3,71 @@ const db = require("../data/db-config") // database access using knex
 
 const router = express.Router()
 
-router.get("/", (req, res, next) => {
+router.get("/", async (req, res, next) => {
+    try {
+        //SQL: `SELECT * FROM POSTS`
+        res.json(await db.select("*").from("posts"))
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get("/:id", async (req, res, next) => {
+    try {
+        //SQL: SELECT * FROM posts WHERE id is param
+        // db("posts").where("id", req.params.id).select()
+        res.json(await db
+            .select("*")
+            .from("posts")
+            .where("id", req.params.id)
+            .first() //returns an object to parse instead of array
+        )
+    } catch (err) {
+        next(err)
+    }
 
 })
 
-router.get("/:id", (req, res, next) => {
+router.post("/", async (req, res, next) => {
+    try {
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents
+        }
+        //SQL: `INSERT INTO posts (title, contents) VALUES(?, ?)`
+        const [id] = await db("posts").insert(payload) //destructures array
+        // res.json({ id }) //returns the id of the newly created row
+        res.json(await db("posts").where("id", id).first()) //returns the new post instead of the id
 
+    } catch (err) {
+        next(err)
+    }
 })
 
-router.post("/", (req, res, next) => {
+router.put("/:id", async (req, res, next) => {
+    const { id } = req.params.id
+    try {
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents
+        }
+        // SQL: `UPDATE posts SET title = ? AND contents = ? WHERE id = ?`
+        await db("posts").where("id", req.params.id).update(payload)
+        res.json(await db("posts").where("id", req.params.id).first()) //returns the new post instead of the id
 
+    } catch (err) {
+        next(err)
+    }
 })
 
-router.put("/:id", (req, res, next) => {
-
-})
-
-router.delete("/:id", (req, res, next) => {
-
+router.delete("/:id", async (req, res, next) => {
+    try {
+        await db("posts").where("id", req.params.id).del()
+        // res.status(204).end() //returns a succesful response with no content
+        res.status(204).end()
+    } catch (err) {
+        next(err)
+    }
 })
 
 module.exports = router
